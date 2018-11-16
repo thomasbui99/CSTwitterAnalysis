6995ae855eed75f0c2f3c827cc8195e7c1b640a7
thomasbui99 <45006047+thomasbui99@users.noreply.github.com>
2018-11-16 10:41:11 +0100

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -7,9 +7,12 @@ from twitter_collect.twitter_connection_setup import twitter_setup
 
 def collect():
     connexion = twitter_setup()
+    tab_tweet=[]
     tweets = connexion.search("Emmanuel Macron",language="french",rpp=100)
     for tweet in tweets:
-        print(tweet.text)
+        #print(tweet.text)
+        tab_tweet += [tweet.text]
+    return(tab_tweet)
 
 
 #information la plus importante : les RT. on recupere le nombre moyen de RT sur le tableau
@@ -23,4 +26,16 @@ def nb_moyen_RT(data):
 def liste_tweets_importants(data,nb_moyen_RT):
     liste= data[data.RTs > nb_moyen_RT].index[0]
 
+def print_data():
+    data = []
+    data [0][0],data [0][1],data [0][2],data [0][3]='','tweet_textual_content','len','RTs'
+    for i in range (1,len(collect())+1):
+        data [i][0],data [i][1],data [i][2],data [i][3]=i-1,collect()[i-1],len(collect()[i-1]),''
+    print(data)
+
+
+
+
+if __name__ == '__main__':
+    print_data()
 
